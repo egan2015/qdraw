c32de892559e01da634c93bc5d19b9c7e6d1e27d
egan2015 <yangang@4g-live.com>
2015-08-01 11:10:36 +0800

    modified:   app/main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -12,11 +12,13 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
 
     MainWindow w;
-    w.show();
+    w.showMaximized();
+/*
     QSize size(QApplication::desktop()->availableGeometry().width(),
                QApplication::desktop()->availableGeometry().height());
     w.resize(size);
     w.move ((QApplication::desktop()->width() - w.width())/2,
             (QApplication::desktop()->height() - w.height())/2);
+*/
     return a.exec();
 }
